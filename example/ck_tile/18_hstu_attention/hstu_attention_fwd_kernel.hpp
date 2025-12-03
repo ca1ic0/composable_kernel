af0f9359a8ce01978f409b60c8e972eaf5c6c42a
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-12-03 08:18:13 +0000

    Clarify the using of kSubQKHeaddim and kQKHeaddim

--- example/ck_tile/18_hstu_attention/hstu_attention_fwd_kernel.hpp
+++ example/ck_tile/18_hstu_attention/hstu_attention_fwd_kernel.hpp
@@ -753,7 +753,7 @@ struct HstuAttentionFwdKernel
 
             return pad_tensor_view(k_dram_naive,
                                    make_tuple(number<HstuAttentionPipeline::kN0>{},
-                                              number<HstuAttentionPipeline::kQKHeaddim>{}),
+                                              number<HstuAttentionPipeline::kSubQKHeaddim>{}),
                                    sequence<false, kPadHeadDimQK>{});
         }();
         const auto v_dram = [&]() {
@@ -788,7 +788,7 @@ struct HstuAttentionFwdKernel
         auto k_dram_window =
             make_tile_window(k_dram,
                              make_tuple(number<HstuAttentionPipeline::kN0>{},
-                                        number<HstuAttentionPipeline::kQKHeaddim>{}),
+                                        number<HstuAttentionPipeline::kSubQKHeaddim>{}),
                              {0, 0});
 
         auto v_dram_window = make_tile_window(
