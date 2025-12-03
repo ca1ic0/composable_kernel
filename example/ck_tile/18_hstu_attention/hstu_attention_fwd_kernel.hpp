7851d05feb2a2be8cf5d4111185c5b9119975d66
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-12-03 09:28:15 +0000

    Further clarification in using kSubQKHeaddim and kQKHeaddim

--- example/ck_tile/18_hstu_attention/hstu_attention_fwd_kernel.hpp
+++ example/ck_tile/18_hstu_attention/hstu_attention_fwd_kernel.hpp
@@ -612,7 +612,7 @@ struct HstuAttentionFwdKernel
             make_tile_window(q_dram,
                              [&]() {
                                  return make_tuple(number<HstuAttentionPipeline::kM0>{},
-                                                   number<HstuAttentionPipeline::kQKHeaddim>{});
+                                                   number<HstuAttentionPipeline::kSubQKHeaddim>{});
                              }(),
                              {i_m0, 0});
 
