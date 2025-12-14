891b1325175aefe18f21ab9f0228bbf5fc887efa
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-12-14 13:50:49 +0000

    Simplifying the codes in defining KDram and QDram tile distribution

--- example/ck_tile/18_hstu_attention/hstu_attention_fwd_pipeline_default_policy.hpp
+++ example/ck_tile/18_hstu_attention/hstu_attention_fwd_pipeline_default_policy.hpp
@@ -148,9 +148,8 @@ struct HstuAttentionFwdPipelineQRKSVSDefaultPolicy
 
         constexpr index_t ElemPerThread = kNPerBlock * kKPerBlock / kBlockSize;
 
-        constexpr index_t MaxVectorSize = 16 / sizeof(VDataType);
-        constexpr index_t kMaxVecLoad   = min(ElemPerThread, MaxVectorSize);
-        constexpr index_t kMinVecLoad   = 4 / sizeof(VDataType);
+        constexpr index_t kMaxVecLoad = Problem::GetVDramTileAccessMaxVectorSize();
+        constexpr index_t kMinVecLoad = 4 / sizeof(VDataType);
 
         // try to avoid writing sub-dword to LDS due to poor performance
         constexpr index_t kVecLoad = ((ElemPerThread / kMaxVecLoad) >= kMinVecLoad)
@@ -291,19 +290,13 @@ struct HstuAttentionFwdPipelineQRKSVSDefaultPolicy
     template <typename Problem>
     CK_TILE_HOST_DEVICE static constexpr auto MakeQDramSingleRepMTileDistribution()
     {
-        using QKVDataType = remove_cvref_t<typename Problem::QKVDataType>;
-
         constexpr index_t kBlockSize = Problem::kBlockSize;
         constexpr index_t kMPerBlock = GetQKBlockGemmSingleRepM<Problem>();
         constexpr index_t kKPerBlock = Problem::HstuAttentionTileSetting::kSubQKHeaddim;
 
-        constexpr index_t MaxVectorSize = 16 / sizeof(QKVDataType);
+        constexpr index_t kKVector = GetAlignmentQ<Problem>();
 
-        constexpr index_t ElemPerThread = (kMPerBlock * kKPerBlock) / kBlockSize;
-        static_assert(0 < ElemPerThread);
-        constexpr index_t kMaxVecLoad = min(ElemPerThread, MaxVectorSize);
-
-        constexpr index_t KPerThread     = kMaxVecLoad;
+        constexpr index_t KPerThread     = kKVector;
         constexpr index_t KThreads       = kKPerBlock / KPerThread;
         constexpr index_t MThreadPerWarp = get_warp_size() / KThreads;
         constexpr index_t NumWarps       = kBlockSize / get_warp_size();
@@ -434,18 +427,13 @@ struct HstuAttentionFwdPipelineQRKSVSDefaultPolicy
     template <typename Problem>
     CK_TILE_HOST_DEVICE static constexpr auto MakeKDramTileDistribution()
     {
-        using QKVDataType = remove_cvref_t<typename Problem::QKVDataType>;
-
         constexpr index_t kBlockSize = Problem::kBlockSize;
         constexpr index_t kNPerBlock = Problem::HstuAttentionTileSetting::kN0Sub;
         constexpr index_t kKPerBlock = Problem::HstuAttentionTileSetting::kSubQKHeaddim;
 
-        constexpr index_t MaxVectorSize = 16 / sizeof(QKVDataType);
-        constexpr index_t ElemPerThread = (kNPerBlock * kKPerBlock) / kBlockSize;
+        constexpr index_t kKVector = GetAlignmentK<Problem>();
 
-        constexpr index_t kMaxVecLoad = min(ElemPerThread, MaxVectorSize);
-
-        constexpr index_t KPerThread     = kMaxVecLoad;
+        constexpr index_t KPerThread     = kKVector;
         constexpr index_t KThreads       = kKPerBlock / KPerThread;
         constexpr index_t NThreadPerWarp = get_warp_size() / KThreads;
         constexpr index_t NumWarps       = kBlockSize / get_warp_size();
